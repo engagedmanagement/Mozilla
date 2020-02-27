05c4e244a6b82d7d5de7ee1817a1e9a377c282e0
engagedmanagement <orgmanagement48@gmail.com>
2020-02-27 13:34:20 -0800

    seventh commit

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,2 +1,10 @@
-const myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello World';
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/firefox-icon.jpeg') {
+      myImage.setAttribute ('src','images/firefox2.jpeg');
+    } else {
+      myImage.setAttribute ('src','images/firefox-icon.jpeg');
+    }
+}
